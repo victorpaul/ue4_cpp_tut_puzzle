265e115a9b304cb4814df852656b7917db46d354
victor <victorpaul.war@gmail.com>
2020-10-08 14:05:08 +0300

    Added replication of the cube

--- Source/app_1/MovingPlatform.cpp
+++ Source/app_1/MovingPlatform.cpp
@@ -3,19 +3,26 @@
 
 #include "MovingPlatform.h"
 
-AMovingPlatform::AMovingPlatform()
-{
+AMovingPlatform::AMovingPlatform() {
 	PrimaryActorTick.bCanEverTick = true;
 	SetMobility(EComponentMobility::Movable);
 }
 
-void AMovingPlatform::Tick(float DeltaTime)
-{
+void AMovingPlatform::BeginPlay() {
+	Super::BeginPlay();
+
+	if (HasAuthority()) {		
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 	if (HasAuthority()) {
 		FVector Location = GetActorLocation();
-		Location += FVector(5 * DeltaTime, 0, 0);
+		Location += FVector(Speed * DeltaTime, 0, 0);
 		SetActorLocation(Location);
 	}
 
--- Source/app_1/MovingPlatform.h
+++ Source/app_1/MovingPlatform.h
@@ -17,6 +17,11 @@ class APP_1_API AMovingPlatform : public AStaticMeshActor
 public:
 	AMovingPlatform();
 
+	virtual void BeginPlay() override;
+
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnyWhere)
+	float Speed = 20;
 	
 };
