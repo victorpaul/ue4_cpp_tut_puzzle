bb1d0f8ed47107bf8d5b5622a2ce3421ee3cb2f1
victor <victorpaul.war@gmail.com>
2020-10-22 15:54:55 +0300

    added game instance

---
+++ Source/app_1/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectIn)
+{
+    UE_LOG(LogTemp, Warning, TEXT("Puzzle game instance constructor"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Puzzle game instance initialization"));
+}
---
+++ Source/app_1/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APP_1_API UPuzzlePlatformsGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+
+    UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectIn);
+
+    virtual void Init() override;
+};
